d58eeebf1ad6e16264ce9d0a90e378e9a5fee82e
Gerardo19D80 <gdaglio@gmail.com>
2024-06-14 20:36:18 -0300

    agregando el fetch a Home

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 
-const Card = ({ name, username, id }) => {
+const Card = ({ cart, setCart}) => {
+
+  const {name, username, id} = cart;
 
   const addFav = ()=>{
     // Aqui iria la logica para agregar la Card en el localStorage
@@ -10,7 +12,9 @@ const Card = ({ name, username, id }) => {
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
-
+        <img src="./images/doctor.jpg" alt="imagensinfoto" />
+        <h4>{name}</h4>
+        <h4>{username}</h4>
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Components/Card'
+import axios from 'axios';
+import "../index.css";
+
+
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  const [cart, setCart] = useState([]);
+  const [doctores, setDoctores] = useState([])
+  const url = 'https://jsonplaceholder.typicode.com/users'
+
+  useEffect(() => {
+    //manera #1 de hacer un fetch
+    // fetch(url)
+    // .then ((response) => { return response.json() })
+    // .then ((data) => { 
+    //     console.log(data);
+    //     setDoctor(data);
+    // })
+    // .catch (err => console.log(err))
+    
+    //manera #2 de hacer un fetch con axios
+    axios(url) 
+        .then ((res) => setDoctores(res.data))
+        .catch (err => console.log(err))
+
+}, [])
+
   return (
     <main className="" >
-      <h1>Home</h1>
+
+      <h1> Selecciona a tu profesional: </h1>
       <div className='card-grid'>
         {/* Aqui deberias renderizar las cards */}
-
         
-
-
+        {doctores.map((doctor) => 
+        <Card cart={doctor} key={doctor.id} setCart={setCart}/>
+        )}
       </div>
     </main>
   )
